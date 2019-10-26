520286f495da4b64dd33834c54463b315e5c0434
Sophia Zhi <sophiazhi@gmail.com>
2019-10-26 17:26:30 -0400

    Revert "navtabs"
    This reverts commit f96c13943904630e0d787133bb5dae93e22fbeae.

--- jetblue/src/App.js
+++ jetblue/src/App.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import './App.css';
-import Review from "./class/Review";
-import Navigation from './class/Navigation';
+import Review from "./class/Review"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      reviews: [
-        {title: "mewqmeqwmwqmewqmewqdsksnksdjnfafjlflfaafll", text: '23213'},
-      ],
+      reviews: [<Review title = "mewqmeqwmwqmewqmewqdsksnksdjnfafjlflfaafll" text = "23213" />],
       display: 'home',
       filters: [],
     };
   }
 
   render() {
-    const reviewItems = this.state.reviews.map((d) => <Review title={d.title} text={d.text} />)
+    const reviewItems = this.state.reviews.map((d) => d)
     console.log(reviewItems);
     return (
       <div>
-        <div>{Navigation()}</div>
-        <div>
-          {reviewItems}
-        </div>
+        {reviewItems}
       </div>
     );
   }
