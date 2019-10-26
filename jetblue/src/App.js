ef352948b42bf1eaee65e1a30578ded65869ff09
Sophia Zhi <sophiazhi@gmail.com>
2019-10-26 15:28:12 -0400

    review and app components

--- jetblue/src/App.js
+++ jetblue/src/App.js
@@ -2,12 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Search JetBlue Reviews
-    </div>
-  );
+class Review extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: 'filler title',
+      text: 'filler text',
+      rating: 'filler rating',
+      source: 'filler source',
+    };
+  }
+
+  render() {
+    return (
+      <div className='review'>
+        <h3>{this.state.title}</h3>
+        <p>{this.state.text}</p>
+        <p>{this.state.source}</p>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reviews: [],
+      display: 'home',
+      filters: [],
+    };
+  }
+
+  render() {
+    return (
+      <div className='header'>JetBlue Reviews</div>
+    );
+  }
 }
 
 export default App;
